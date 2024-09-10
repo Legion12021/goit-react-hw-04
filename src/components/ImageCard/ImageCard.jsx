02e16ada1dd9e04c0eeaf344d8a71dddfbcc74ae
Legion12021 <146947925+Legion12021@users.noreply.github.com>
2024-09-10 18:15:06 +0300

    done

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,16 @@
+import css from "./ImageCard.module.css";
+
+function ImageCard({ description, small, regular, openModal }) {
+  return (
+    <div>
+      <img
+        className={css.searchImg}
+        src={small}
+        alt={description}
+        onClick={() => openModal(regular, description)}
+      />
+    </div>
+  );
+}
+
+export default ImageCard;
